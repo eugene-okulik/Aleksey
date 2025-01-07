7c1b7d5d7234f71cbf9c57bc007aa1eb20c1e28b
Prince Aleksey <princeaa@aqa-engineer.ru>
2025-01-07 23:28:00 +0300

    добавил разные варианты теÐмператур

---
+++ test.py
@@ -0,0 +1,102 @@
+# num1, num2 = map(int, input().split())
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+
+
+# monitor = int(input())
+# block = int(input())
+# mouse = int(input())
+# keyboard = int(input())
+# pc_count = int(input())
+#
+# print('Стоимость всех компов:', (monitor + block + mouse + keyboard) * pc_count)
+
+
+# candy_malek = int(input())
+# candy_liz = int(input())
+# candy_director = int(input())
+#
+# print(candy_liz + candy_director)
+# print(candy_malek - (candy_liz + candy_director))
+
+
+# hour_sleep = int(input())
+# if hour_sleep > 7:
+#     print('Молодец, ты отлично выспался!')
+
+# subscriber_one = int(input())
+# subscriber_two = int(input())
+#
+# summ_subscriber = subscriber_one + subscriber_two
+# if summ_subscriber > 1000:
+#     print('Ютуб растёт!')
+# else:
+#     print('Ютуб замедляется...')
+
+# password = input()
+# if password == 'жукбук34':
+#     print('Добро пожаловать в систему!')
+# if password != 'жукбук34':
+#     print('Пароль неверный. Отказ в доступе!')
+
+# if input() != input():
+#     print('Добро пожаловать в систему!')
+
+# # пользователь вводит названия напитка
+# drink = input()
+#
+# # если введено чай
+# if drink == 'чай':
+#     print('Добавь лимон!')
+#
+# # если введено кофе
+# if drink == 'кофе':
+#     print('Добавь сахар!')
+
+# favorite_hero = input()
+# if favorite_hero == 'Халк':
+#     print('Халк готов к разгромам!')
+# if favorite_hero == 'Тор':
+#     print('Тор заказывает громовую бурю!')
+
+
+# name = input()
+# if name == 'Малёк':
+#     print('Здравствуй, Малёк, я так скучала!')
+# else:
+#     print('Уходите, я вас не знаю!')
+
+# game_1 = int(input())
+# game_2 = int(input())
+# game_3 = int(input())
+#
+# summ_game = game_3 + game_2 + game_1
+# if summ_game < 4:
+#     print('Можно ещё поиграть!')
+# else:
+#     print('Игр достаточно, лучше пойти кодить!')
+
+
+# pie_price = int(input())
+# juice_price = int(input())
+# all_my_money = int(input())
+#
+# if pie_price + juice_price < all_my_money:
+#     print('Ура, мы вкусно покушаем!')
+# else:
+#     print('Денег не хватает, сегодня без обеда.')
+
+# day = input()
+# if day == 'будни':
+#     print('Добыть завтрак\nСходить в школу\nПопрогать')
+# else:
+#     print('Посмотреть любимый сериал\nПогулять с друзьями\nСыграть в настолки')
+
+
+learn_python = input()
+two_course = input()
+if learn_python == 'да':
+    print('Круто, тебя ждут крутые проекты и большая зарплата!')
+if two_course == 'да':
+    print('Класс, значит, скоро ты напишешь свою первую игру!')
